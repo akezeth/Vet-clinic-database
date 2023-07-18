e8380c38976e2267b472827ec05b562810532e2c
Aklilu Mekonnen <akezeth@gmail.com>
2023-07-18 03:39:57 -0700

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,3 +1,31 @@
-/*Queries that provide answers to the questions from all projects.*/
+select *
+from animals
+where name like '%mon';
 
-SELECT * from animals WHERE name = 'Luna';
+select name
+from animals
+where date_of_birth >= '2016-01-01' and date_of_birth <= '2019-12-31';
+
+select name
+from animals
+where neutered = '1' and escape_attempts < 3;
+
+select date_of_birth
+from animals
+where name = 'Agumon' or name= 'Pikachu';
+
+select name, escape_attempts
+from animals
+where weight_kg > 10.5;
+
+select *
+from animals
+where neutered = '1';
+
+select *
+from animals
+where name != 'Gabumon';
+
+select *
+from animals
+where weight_kg between 10.4 and 17.3;
