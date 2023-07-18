f6f7d56adb54ed05fa110dc5f96b8d88ba6bacc0
Aklilu Mekonnen <akezeth@gmail.com>
2023-07-17 23:28:29 -0700

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,8 @@
-/* Database schema to keep the structure of entire database. */
-
-CREATE TABLE animals (
-    name varchar(100)
+create table animals (
+	id integer GENERATED ALWAYS AS IDENTITY,
+	name varchar(50),
+	date_of_birth date,
+	escape_attempts integer,
+	neutered boolean,
+	weight_kg decimal
 );
