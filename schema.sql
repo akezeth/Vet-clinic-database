80cceeb1054b2c3776abb59e0019ee39181a8e99
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-20 21:01:14 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -8,4 +8,39 @@ create table animals (
 );
 
 alter table animals 
-	add species varchar(10);
+add species varchar(10);
+
+create table owners(
+	id integer GENERATED ALWAYS AS IDENTITY,
+	full_name varchar(100),
+	age integer,
+	primary key(id)
+);
+
+create table species(
+	id integer GENERATED ALWAYS AS IDENTITY,
+	name varchar(50),
+	primary key(id)
+);
+
+alter table animals
+add primary key(id);
+
+alter table animals
+drop column species;
+
+alter table animals
+add column species_id integer;
+
+alter table animals
+add foreign key(species_id)
+references species(id)
+on delete cascade;
+
+alter table animals
+add column owners_id integer;
+
+alter table animals
+add foreign key(owners_id)
+references owners(id)
+on delete cascade;
