f83ec6a5d0ec094a683ba30b39d9323757fbe524
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-20 21:01:48 +0300

    Update data.sql

--- data.sql
+++ data.sql
@@ -16,4 +16,47 @@ values
 	('Boarmon', '2005-06-07', 7, '1', 20.4),
 	('Blossom', '1998-10-13', 3, '1', 17),
 	('Ditto', '2022-05-14', 4, '1', 22);
-  
+
+insert into owners
+	(full_name, age)
+values
+	('Sam Smith', 34),
+	('Jennifer Orwell', 19),
+	('Bob', 45),
+	('Melody Pond', 77),
+	('Dean Winchester', 14),
+	('Jodie Whittaker', 38);
+	
+insert into species
+	(name)
+values
+	('Pokemon'),
+	('Digimon');
+	
+update animals
+set species_id = 2 
+where name like '%mon';
+
+update animals
+set species_id = 1 
+where species_id is null;
+
+update animals
+set owners_id = 1
+where name = 'Agumon';
+
+update animals
+set owners_id = 2
+where name = 'Pikachu' or name = 'Gabumon';
+
+update animals
+set owners_id = 3
+where name = 'Devimon' or name = 'Plantmon';
+
+update animals
+set owners_id = 4
+where name = 'Charmander' or name = 'Squirtle' or name = 'Blossom';
+
+update animals
+set owners_id = 5
+where name = 'Angemon' or name = 'Boarmon';
