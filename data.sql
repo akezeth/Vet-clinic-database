47b2a42809cfd0fed28934744602d79a2d7ff74a
Enoisong <enobongitiaba988@gmail.com>
2023-07-24 06:16:01 -0700

    update data sql file

--- data.sql
+++ data.sql
@@ -124,3 +124,8 @@ values
 	 (select id from vets where name = 'Stephanie Mendez'), '2020-05-24'),
     ((select id from animals where name = 'Blossom'),
 	 (select id from vets where name = 'William Tatcher'), '2021-01-11');
+
+	 
+ INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+ insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
