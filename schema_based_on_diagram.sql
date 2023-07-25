67f23156b14969ee0c3044123d3853829d7150a3
akezeth <116703083+akezeth@users.noreply.github.com>
2023-07-25 18:16:03 +0300

    Create index for foreign keys

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -30,7 +30,8 @@ create table invoice_items (
 	unit_price decimal,
 	quantity integer,
 	total_price decimal,
-	invoice_id integer not null foreign key references invoices(id)
+	invoice_id integer not null foreign key references invoices(id),
+	treatment_id integer not null foreign key references treatments(id)
 );
 
 
@@ -38,6 +39,15 @@ create table treatment_histories (
 	medical_history_id integer not null foreign key references medical_histories(id),
 	treatment_id integer not null foreign key references treatments(id)
 );
+
+create index medical_histories_patient_id on medical_histories(patient_id);
+create index invoices_medical_history_id on invoices(medical_history_id);
+create index invoices_item_invoice_id on invoice_items(invoice_id);
+create index invoices_item_treatment_id on invoice_items(treatment_id);
+create index treatment_histories_medical_history_id on treatment_histories(medical_history_id);
+create index treatment_histories_treatment_id on treatment_histories(treatment_id);
+
+
 
 
 
